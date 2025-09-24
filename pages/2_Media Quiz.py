c877ad2502776d730488667e5abe7fa4ed2ea72d
Winston Wu <161237354+winstonxwu@users.noreply.github.com>
2025-09-24 11:49:20 -0400

    couch update

--- pages/2_Media Quiz.py
+++ pages/2_Media Quiz.py
@@ -13,6 +13,7 @@ def quiz_section():
         st.write(f"{movie_genre}! My favorite genre is Comedy too, and my personal favorite is Dumb and Dumber!")
     else:
         st.write(f"{movie_genre}'s an interesting choice! I prefer Comedy, though.")
+
     num_movies = st.slider(f"How many {movie_genre} movies have you watched this year?", 0, 100, 10) #NEW
     if num_movies > 75:
         st.write("Wow, so many!")
@@ -20,7 +21,9 @@ def quiz_section():
         st.write("Just like me!")
     else:
         st.write("You must like movies a lot!")
+
     st.write('---')
+
     st.subheader("2. What's your favorite genre of music?")
     muImage = info.music_image
     st.image(muImage, width=200)
@@ -28,7 +31,9 @@ def quiz_section():
     st.write(f"You've got good taste!")
     if music_genre == 'Rap':
         st.write("That's my favorite, too! My favorite artists are MF DOOM and Mac Miller.")
+
     st.write('---')
+
     st.subheader("3. What are your favorite book genres?")
     bImage = info.book_image
     st.image(bImage, width=200)
@@ -37,16 +42,20 @@ def quiz_section():
         st.write("I love fantasy books too! Have you read Eragon yet? The movies aren't great, but the books are amazing!")
     else:
         st.write(f"I don't read as much as I used to, but those might be worth checking out!")
+
     st.write('---')
+
     st.subheader("4. What's your favorite place to relax and enjoy a movie/album/book?")
     relax_place = st.text_input("Type your answer here:") #NEW
-    if relax_place == "couch":
+    if relax_place.lower() == "couch":
         st.write("Same here! There's nothing better than my couch.")
     elif relax_place:
         st.write(f"{relax_place} sounds like a great place to unwind! I prefer my couch.")
     else:
         st.write("Finding a good place to relax is important!")
+
     st.write('---')
+
     st.subheader("5. What's your favorite thing to snack on while relaxing?")
     snack = st.selectbox("Choose a snack:", ('Popcorn', 'Chips', 'Chocolate', 'Fruit', 'Pizza', 'Candy', 'Other'))
     if snack == "Pizza":
@@ -59,6 +68,7 @@ def quiz_section():
             st.write("I'm curious to know what your favorite snack is!")
     else:
         st.write(f"{snack} is a solid choice!")
+
     st.write('---')
 
 
