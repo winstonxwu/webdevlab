a9b42913ed7065fdb23e1c6777c24749339d5a2f
Winston Wu <161237354+winstonxwu@users.noreply.github.com>
2025-09-23 20:11:59 -0400

    lab 01

---
+++ Home_Page.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab01")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Web Development - Section C")
+st.subheader("Winston Wu")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. **Winston's Portfolio**: A portfolio page to show my background, experience, and skills.
+
+""")
+
---
+++ info.py
@@ -0,0 +1,76 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "/Users/winstonxwu/Downloads/WebDevLab01/WebDevLab01/Images/RPI.JPG"
+about_me = "I'm Winston Wu, a first-year CS student at Georgia Tech. I'm from Painted Post, NY, and I love web development!"
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/winston-wu-541894313/"
+my_github_url = "https://github.com/winstonxwu"
+my_email_address = "winwu2007@gmail.com"
+
+
+education_data = {
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2029',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "MATH 1554"], 
+    "names":["Intro to CS", "Linear Algebra"], 
+    "semester_taken":["1st", "1st"],
+    "skills":["Python is really fun! (It's better than Java ...)", "Why are the numbers in boxes?"],
+    }
+experience_data = {
+    "Software Engineering Intern at Corning Incorporated": (["- Designed and produced imaging system for analysis of x-ray irradiated and damaged glass samples with precision of ~10 nm",
+                                                                          "- Utilized OpenCV technology to color and search for errors and features in glass, decreasing glass feature analysis time by 30%"],"/Users/winstonxwu/Downloads/WebDevLab01/WebDevLab01/Images/maxresdefault.jpg"),
+    "Math Club Member":(["- Participated in Corning Community College math competition",
+                                                           "- Learned about game theory, statistics, and algebra"],"/Users/winstonxwu/Downloads/WebDevLab01/WebDevLab01/Images/FH5FVJYVkActjne.jpg"),
+    "Yearbook Editor":(["- Cleaned and organized data for student portraits and names"],"Images/5xggk3cthy731.jpg")
+
+}
+
+projects_data = {
+    "Name Guesser Project": "- Developed ML model in PyTorch to detect language of origin of names with 0.4 average batch loss, trained on dataset of 17,063 names spanning 18 languages"
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 70,
+    "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"Cantonese": "🇨🇳",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Proficient",
+    "Cantonese": "Fluent"
+}
+leadership_data = {
+    "President of Corning-Painted Post High School Coding Club": (["Taught 50+ students introductory Python"],"/Users/winstonxwu/Downloads/WebDevLab01/WebDevLab01/Images/1723765595210.jpeg"),
+
+}
+activity_data={
+    "Rose's Youth Philanthropists": ["- Reviewed grant proposals for non-profits in the Corning-Elmira area", 
+            "- Helped distribute $12,000 in funds to organizations like Corning Food Pantry"]
+}
